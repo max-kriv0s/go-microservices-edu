947c27600868791795c92c22852853b4f9216895
Maxim Krivoshein <max.kriv0s@gmail.com>
2025-11-19 00:57:29 +0300

    fix: fix error after code review

--- assembly/internal/service/consumer/order_consumer/handler.go
+++ assembly/internal/service/consumer/order_consumer/handler.go
@@ -32,35 +32,36 @@ func (s *service) OrderPaidHandler(ctx context.Context, msg kafkaConsumer.Messag
 		zap.String("transaction_uuid", event.TransactionUUID),
 	)
 
-	go func(event model.OrderPaidEvent) {
-		n, err := rand.Int(rand.Reader, big.NewInt(10)) // 0..9
-		if err != nil {
-			logger.Error(ctx, "Failed to generate random delay", zap.Error(err))
-			return
-		}
-		delay := time.Duration(n.Int64()+1) * time.Second // 1..10 секунд
+	n, err := rand.Int(rand.Reader, big.NewInt(10)) // 0..9
+	if err != nil {
+		logger.Error(ctx, "Failed to generate random delay", zap.Error(err))
+		return err
+	}
+	delay := time.Duration(n.Int64()+1) * time.Second // 1..10 секунд
 
-		select {
-		case <-time.After(delay):
-			// Таймер сработал, продолжаем
-		case <-ctx.Done():
-			// Контекст отменён, выходим
-			logger.Info(ctx, "Order processing cancelled")
-			return
-		}
+	timer := time.NewTimer(delay)
+	defer timer.Stop()
 
-		shipAssemblyEvent := model.ShipAssembledEvent{
-			EventUUID:    uuid.NewString(),
-			OrderUUID:    event.OrderUUID,
-			UserUUID:     event.UserUUID,
-			BuildTimeSec: int64(delay.Seconds()),
-		}
+	select {
+	case <-timer.C:
+		// Таймер сработал, продолжаем
+	case <-ctx.Done():
+		// Контекст отменён, выходим
+		logger.Info(ctx, "Order processing cancelled")
+		return ctx.Err()
+	}
 
-		err = s.orderProducerService.ProduceShipAssembled(ctx, shipAssemblyEvent)
-		if err != nil {
-			logger.Error(ctx, "Failed to produce ShipAssembled", zap.Error(err))
-		}
-	}(event)
+	shipAssemblyEvent := model.ShipAssembledEvent{
+		EventUUID:    uuid.NewString(),
+		OrderUUID:    event.OrderUUID,
+		UserUUID:     event.UserUUID,
+		BuildTimeSec: int64(delay.Seconds()),
+	}
+
+	err = s.orderProducerService.ProduceShipAssembled(ctx, shipAssemblyEvent)
+	if err != nil {
+		logger.Error(ctx, "Failed to produce ShipAssembled", zap.Error(err))
+	}
 
 	return nil
 }
